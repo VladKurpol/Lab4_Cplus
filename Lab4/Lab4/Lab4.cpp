74de8e94b2330962f67b4d299ed47aace099dacc
VladKurpol <48331088+VladKurpol@users.noreply.github.com>
2019-04-26 00:52:38 +0300

    sadasd

---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,31 @@
+﻿// Lab4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	char string[] = "Hello World!";
+	char example[] = { '\0', ' ', ',' , '.' };
+
+	int pointer = 1;
+	int length_of_ex = strlen(example);
+	int length = strlen(string);
+	std::cout << length << "\n";
+
+
+	for (size_t i = 0; i <= length; i++)
+	{
+		if (string[0] == string[i])
+		{
+
+		}
+		else if ((string[0] != string[i]))
+		{
+			pointer++;
+		}
+	}
+
+	std::cout << pointer;
+}
