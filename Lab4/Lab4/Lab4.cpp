e867720023bb10d4884f26939f60e1fe18032279
VladKurpol <48331088+VladKurpol@users.noreply.github.com>
2019-04-26 01:27:21 +0300

    dsfsfd

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -3,29 +3,12 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <set>
 
 int main()
 {
-	char string[] = "Hello World!";
-	char example[] = { '\0', ' ', ',' , '.' };
-
-	int pointer = 1;
-	int length_of_ex = strlen(example);
-	int length = strlen(string);
-	std::cout << length << "\n";
-
-
-	for (size_t i = 0; i <= length; i++)
-	{
-		if (string[0] == string[i])
-		{
-
-		}
-		else if ((string[0] != string[i]))
-		{
-			pointer++;
-		}
-	}
-
-	std::cout << pointer;
+	std::string str{ "voyVoy polegche" };
+	std::set<char> letters{ str.begin(), str.end() };
+	std::cout << letters.size() << "\n";
 }
